5932acb264b891088d3ffd368506865d1bee7858
John-Alexander Kamau <133286098+Xander-AJ@users.noreply.github.com>
2023-09-17 21:33:13 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<johnalexanderkamau>:<r814P3P8QCRFwvmd>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<johnalexanderkamau>:<r814P3P8QCRFwvmd>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<johnalexanderkamau>:<r814P3P8QCRFwvmd>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<johnalexander>:<S0TOlAVTnJahn53j>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<johnalexander>:<S0TOlAVTnJahn53j>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<johnalexander>:<S0TOlAVTnJahn53j>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
